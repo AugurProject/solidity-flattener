714c4c02624c5a159b82bc5e19bd6a1865050215
nuevoalex <achap5dk@gmail.com>
2020-01-06 16:39:09 -0800

    update

--- setup.py
+++ setup.py
@@ -3,11 +3,11 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='solidity-flattener',
+    name='augur-solidity-flattener',
     description='Flattens Solidity code that uses imports into a single file.',
     author='Eric Huang, BlockCAT Technologies Inc.',
     author_email='team@blockcat.io',
-    url='https://github.com/BlockCatIO/solidity-flattener',
+    url='https://github.com/AugurProject/solidity-flattener',
     version='0.2.4',
     packages=find_packages(exclude=["*tests"]),
     scripts=['bin/solidity_flattener']
